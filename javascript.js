57fd85dcd404c69fadf7ecbd7a3c4533a37d77d3
Quantum <vinnykillia@gmail.com>
2026-07-01 22:01:16 -0400

    added html and css

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+     display.value += input;
+     console.log(display);
+     
+}
+
+function clearDisplay() {
+
+}
+
+function calculate() {
+
+}
